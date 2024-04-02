9a024134c026cf513850d22f53d3a31628ca6483
Juliet Shin <juliet.shin@ucop.edu>
2024-04-01 17:12:19 -0700

    deleted tmp.js

--- tmp.js
+++
@@ -1,9 +0,0 @@
-// Constants for the development environment
-export const DMPTOOL_NAME = 'DMPTool';
-export const DMPTOOL_URL = 'https://dmptool-dev.cdlib.org';
-
-export const API_BASE_URL = 'https://api.dmphub.uc3dev.cdlib.net';
-export const COGNITO_BASE_URL = 'https://auth.dmphub.uc3dev.cdlib.net';
-// export const APP_REDIRECT_URI = 'https://api.dmphub.uc3dev.cdlib.net/oauth_callback';
-export const APP_REDIRECT_URI = 'https://dmphub.uc3dev.cdlib.net/authenticate/';
-export const APP_CLIENT_ID = '52d81jm7pv267t1073jlj6u4t1'
