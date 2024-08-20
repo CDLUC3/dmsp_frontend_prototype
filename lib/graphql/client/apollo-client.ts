bc8bc47e7d65940411737b728e34248a26bd950c
Juliet Shin <juliet.shin@ucop.edu>
2024-08-20 10:13:28 -0700

    updated apollo-client to pass auth token header in requests

--- lib/graphql/client/apollo-client.ts
+++ lib/graphql/client/apollo-client.ts
@@ -1,8 +1,53 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { ApolloClient, InMemoryCache, createHttpLink, from } from "@apollo/client";
+import { onError } from '@apollo/client/link/error';
+import { createAuthLink } from '@/utils/authLink';
+
+const httpLink = createHttpLink({
+    uri: `${process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT}/graphql`
+});
+
+const authLink = createAuthLink();
+
+interface CustomError extends Error {
+    customInfo?: { customMessage: string }
+}
+
+const errorLink = onError(({ graphQLErrors, networkError, operation }) => {
+    if (graphQLErrors) {
+        graphQLErrors.forEach(({ message, extensions }) => {
+            //Check for specific error codes
+            switch (extensions?.code) {
+                case 'UNAUTHORIZED':
+                    console.error('Unauthorized');
+                    break;
+                case 'FORBIDDEN':
+                    console.error('Forbidden');
+                    break;
+                case 'GRAPHQL_VALIDATION_FAILED':
+                    extensions.customInfo = { customMessage: "There was a problem. Please try again" };
+                    operation.setContext({ extensions })
+                    break;
+                case 'INTERNAL_SERVER_ERROR':
+
+                    break;
+                default:
+                    console.error(`[GraphQL Error]: ${message}`)
+                    break;
+            }
+        })
+    }
+
+    if (networkError) {
+        const customNetworkError = networkError as CustomError;
+
+        customNetworkError.customInfo = { customMessage: 'There was a problem ' };
+        operation.setContext({ networkError: customNetworkError });
+    }
+});
 
 export const createApolloClient = () => {
     return new ApolloClient({
-        uri: `${process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT}/graphql`,
+        link: from([errorLink, authLink, httpLink]),
         cache: new InMemoryCache(),
     });
 };
