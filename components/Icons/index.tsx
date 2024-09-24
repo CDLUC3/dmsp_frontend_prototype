959f3e2440d4595eabbd35148020e03bdd25a980
Andre Engelbrecht <andre@teh-node.co.za>
2024-09-24 15:53:56 +0100

    (#80) Added tests for DmpIcon
    - Also added npm runner for jest without extra commands so that we can
      run custom ad-hoc jest commands when needed; Like limiting tests or
      clearing cache.
    
    - Silence some typescript errors for editor

--- components/Icons/index.tsx
+++ components/Icons/index.tsx
@@ -9,12 +9,16 @@ interface IconProps {
 export function DmpIcon({ icon }: IconProps) {
   return (
     <svg
+      data-testid="dmpIconSvg"
       className={`dmp-icon icon-${icon}`}
       width="24px"
       height="24px"
       fill="#5f6368"
     >
-      <use href={`/icons/iconset.svg#icon-${icon}`} />
+      <use
+        href={`/icons/iconset.svg#icon-${icon}`}
+        data-testid="dmpIconSvgUse"
+      />
     </svg>
   )
 }
