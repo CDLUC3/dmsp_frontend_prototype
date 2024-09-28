1415ae2e57b861fec05743c35ab2899c8dbb11d9
Juliet Shin <juliet.shin@ucop.edu>
2024-09-27 20:43:35 -0700

    Added back 'credentials: include' to the fetch options in order to get csrf token

--- utils/authHelper.ts
+++ utils/authHelper.ts
@@ -3,7 +3,9 @@ import logECS from '@/utils/clientLogger';
 
 export const refreshAuthTokens = async () => {
   try {
-    const response = await fetch(`${process.env.NEXT_PUBLIC_SERVER_ENDPOINT}/apollo-refresh`);
+    const response = await fetch(`${process.env.NEXT_PUBLIC_SERVER_ENDPOINT}/apollo-refresh`, {
+      credentials: 'include',// This is needed or else the frontend can't access the csrf token in the header
+    });
     const message = await response.json();
     return { response, message };
   } catch (err) {
@@ -17,7 +19,9 @@ export const refreshAuthTokens = async () => {
 //Function to fetch CSRF token from the backend
 export const fetchCsrfToken = async () => {
   try {
-    const response = await fetch(`${process.env.NEXT_PUBLIC_SERVER_ENDPOINT}/apollo-csrf`);
+    const response = await fetch(`${process.env.NEXT_PUBLIC_SERVER_ENDPOINT}/apollo-csrf`, {
+      credentials: 'include',// This is needed or else the frontend can't access the csrf token in the header
+    });
     return response;
   } catch (err) {
     logECS('error', `Error getting csrf token from backend: ${err}`, {
