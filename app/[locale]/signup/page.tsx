0b499c4f1255e84e422168f65ab6cc107935caa7
Andre Engelbrecht <andre@teh-node.co.za>
2024-10-09 10:14:27 +0100

    (#117) Started work on login and signup pages

--- app/[locale]/signup/page.tsx
+++ app/[locale]/signup/page.tsx
@@ -1,7 +1,16 @@
 'use client'
 
-import React, {useEffect, useRef, useState} from 'react';
-import {useRouter} from 'next/navigation';
+import React, { useEffect, useState, useRef } from 'react';
+import { useRouter } from 'next/navigation';
+import {
+  Button,
+  Form,
+  TextField,
+  Label,
+  Input,
+  FieldError,
+  Checkbox,
+} from "react-aria-components";
 import styles from './signup.module.scss';
 import {useCsrf} from '@/context/CsrfContext';
 import logECS from '@/utils/clientLogger';
@@ -11,7 +20,7 @@ import {useAuthContext} from '@/context/AuthContext';
 const SignUpPage: React.FC = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
-    const [acceptedTerms, setAcceptedTerms] = useState<number>(0);
+    const [termsAccepted, setTermsAccepted] = useState<bool>(false);
     const [errors, setErrors] = useState<string[]>([]);
     const [loading, setLoading] = useState(false);
     const router = useRouter();
@@ -19,109 +28,164 @@ const SignUpPage: React.FC = () => {
     const { csrfToken } = useCsrf();
     const { setIsAuthenticated } = useAuthContext();
 
-    const handleAcceptedTerms = async (event: React.ChangeEvent<HTMLInputElement>) => {
-        event.preventDefault();
+    const handleSignUp = async (ev: React.FormEvent) => {
+      ev.preventDefault();
 
-        if (event.target.checked) {
-            setAcceptedTerms(1)
-        }
-    }
+      setLoading(true);
+      setErrors([]);
 
-    const handleSignUp = async (event: React.FormEvent) => {
-        event.preventDefault();
-        setLoading(true);
-        setErrors([]); // Clear previous errors
+      let data = {
+        first_name: ev.target.first_name.value,
+        last_name: ev.target.last_name.value,
+        email: ev.target.email.value,
+        password: ev.target.password.value,
+        "acceptedTerms": termsAccepted,
+      };
 
+      console.log(data);
 
-        const signupRequest = async (token: string | null) => {
-            return await fetch(`${process.env.NEXT_PUBLIC_SERVER_ENDPOINT}/apollo-signup`, {
-                method: 'POST',
-                credentials: 'include',
-                headers: {
-                    'Content-Type': 'application/json',
-                    'X-CSRF-TOKEN': token || '',
-                },
-                body: JSON.stringify({ email, password, acceptedTerms }),
-            });
-        }
+       const signupRequest = async (token: string | null) => {
+         return await fetch(`${process.env.NEXT_PUBLIC_SERVER_ENDPOINT}/apollo-signup`, {
+           method: 'POST',
+           credentials: 'include',
+           headers: {
+             'Content-Type': 'application/json',
+             'X-CSRF-TOKEN': token || '',
+           },
+           body: JSON.stringify(data),
+         });
+       }
 
-        if (csrfToken) {
-            /* eslint-disable @typescript-eslint/no-explicit-any */
-            try {
-                const response = await signupRequest(csrfToken);
+      /* eslint-disable @typescript-eslint/no-explicit-any */
+      try {
+        const response = await signupRequest(csrfToken);
 
-                if (response && response.ok) {
-                    setIsAuthenticated(true);
-                    router.push('/')
-                } else {
-                    await handleErrors(response, signupRequest, setErrors, router, '/signup');
-                }
-
-            } catch (err: any) {
-                logECS('error', 'Signup error', {
-                    error: err,
-                    url: { path: '/apollo-signup' }
-                });
-            } finally {
-                setLoading(false);
-            }
+        if (response && response.ok) {
+          setIsAuthenticated(true);
+          router.push('/')
         } else {
-            setErrors(prev => prev.concat('Something went wrong'))
+          await handleErrors(response, signupRequest, setErrors, router, '/signup');
         }
 
+        // if (csrfToken) {
+        //     /* eslint-disable @typescript-eslint/no-explicit-any */
+        //     try {
+        //         const response = await signupRequest(csrfToken);
+
+        //         if (response && response.ok) {
+        //             setIsAuthenticated(true);
+        //             router.push('/')
+        //         } else {
+        //             await handleErrors(response, signupRequest, setErrors, router, '/signup');
+        //         }
+
+        //     } catch (err: any) {
+        //         logECS('error', 'Signup error', {
+        //             error: err,
+        //             url: { path: '/apollo-signup' }
+        //         });
+        //     } finally {
+        //         setLoading(false);
+        //     }
+        // } else {
+        //     setErrors(prev => prev.concat('Something went wrong'))
+        // }
+
+      } catch (err: any) {
+        logECS('error', 'Signup error', {
+          error: err,
+          url: { path: '/apollo-signup' }
+        });
+      } finally {
+        setLoading(false);
+      }
     };
 
     useEffect(() => {
-        if (errors) {
-            if (errorRef.current) {
-                errorRef.current.scrollIntoView({ behavior: 'smooth' });
-                errorRef.current.focus();
-            }
+      if (errors) {
+        if (errorRef.current) {
+          errorRef.current.scrollIntoView({ behavior: 'smooth' });
+          errorRef.current.focus();
         }
-    }, [errors])
+      }
+    }, [errors]);
 
     return (
-        <div className={styles.signupWrapper} ref={errorRef}>
-            <form className={styles.signupForm} id="signup" onSubmit={handleSignUp}>
-                {errors && errors.length > 0 &&
-                    <div className="error">
-                        {errors.map((error, index) => (
-                            <p key={index}>{error}</p>
-                        ))}
-                    </div>
-                }
-                <h3 className={styles.heading3}>Create an account</h3>
+      <div className={styles.signupWrapper} ref={errorRef}>
+        <Form className={styles.signupForm} onSubmit={handleSignUp}>
+          {errors && errors.length > 0 &&
+            <div className="error">
+                {errors.map((error, index) => (
+                    <p key={index}>{error}</p>
+                ))}
+            </div>
+          }
+          <h3 className={styles.heading3}>Create an account</h3>
 
-                <label htmlFor="email">Email address:</label>
-                <input
-                    id="email"
-                    type="email"
-                    name="email"
-                    value={email}
-                    onChange={e => setEmail(e.target.value)}
-                    required />
+          <TextField
+            name="first_name"
+            type="input"
+            aria-label="First Name"
+            isRequired
+          >
+            <Label>First Name</Label>
+            <FieldError />
+            <Input />
+          </TextField>
 
-                <label htmlFor="password">Password:</label>
-                <input
-                    id="password"
-                    type="password"
-                    name="password"
-                    value={password}
-                    onChange={e => setPassword(e.target.value)}
-                    required />
+          <TextField
+            name="last_name"
+            type="input"
+            aria-label="Last Name"
+            isRequired
+          >
+            <Label>Last Name</Label>
+            <FieldError />
+            <Input />
+          </TextField>
 
-                <label htmlFor="acceptedTerms">Accept terms:</label>
-                <input
-                    type="checkbox"
-                    id="acceptedTerms"
-                    name="acceptedTerms"
-                    onChange={e => handleAcceptedTerms(e)}
-                />
+          Institution, Cannot find institution checkbox
 
-                <button type="submit" id="signup-button" disabled={loading}>Sign up</button>
-            </form>
+          <TextField
+            name="email"
+            type="email"
+            aria-label="Email address"
+            isRequired
+          >
+            <Label>Email address</Label>
+            <FieldError />
+            <Input />
+          </TextField>
 
-        </div>
+          <TextField
+            name="password"
+            type="password"
+            aria-label="Password"
+            isRequired
+          >
+            <Label>Password</Label>
+            <FieldError />
+            <Input />
+          </TextField>
+
+          <Checkbox isSelectec={termsAccepted} onChange={setTermsAccepted}>
+            <div className="checkbox">
+              <svg viewBox="0 0 18 18" aria-hidden="true">
+                <polyline points="1 9 7 14 15 4" />
+              </svg>
+            </div>
+            Accept terms?
+          </Checkbox>
+
+          <Button
+            type="submit"
+            isDisabled={(loading || !termsAccepted)}
+          >
+            {loading ? 'Signing up ...' : 'Sign Up'}
+          </Button>
+        </Form>
+      </div>
     );
 };
+
 export default SignUpPage;
