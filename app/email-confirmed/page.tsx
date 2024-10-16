e1d9928b9090b774ce6f1a12e43a6c9ff60cd0f3
Juliet Shin <juliet.shin@ucop.edu>
2024-10-16 16:50:26 -0700

    Added ContentContainer and fixed some unit tests

--- app/email-confirmed/page.tsx
+++ app/email-confirmed/page.tsx
@@ -6,6 +6,7 @@ import {
   Button
 } from 'react-aria-components';
 import PageWrapper from '@/components/PageWrapper';
+import ContentContainer from '@/components/ContentContainer';
 
 
 const EmailConfirmed: React.FC = () => {
@@ -17,10 +18,12 @@ const EmailConfirmed: React.FC = () => {
   return (
     <PageWrapper title={'Email confirmed'}>
       <h1>Email confirmed</h1>
-      <div className="container">
-        <p>Thanks for confirming your alias email. Click below to log in and access your plans. Remember you still need to use your primary email to log in.</p>
-        <Button data-primary onPress={handleGoToLogin}>Log in</Button>
-      </div>
+      <ContentContainer>
+        <div className="container">
+          <p>Thanks for confirming your alias email. Click below to log in and access your plans. Remember you still need to use your primary email to log in.</p>
+          <Button data-primary onPress={handleGoToLogin}>Log in</Button>
+        </div>
+      </ContentContainer>
     </PageWrapper>
   )
 }
