59dfb0c1069a8e33e2eee483c43b3f6f7992ae42
Juliet Shin <juliet.shin@ucop.edu>
2024-11-19 13:00:19 -0800

    isolated translations tests just to temporary locale-test page. Also, created a generateCrowdinConfig.js script to get env variables and generate the crowdin.yml file that will be used for uploading and downloading content, due to sensitive token inside

--- app/[locale]/500-error/page.tsx
+++ app/[locale]/500-error/page.tsx
@@ -1,12 +1,10 @@
 // pages/500.js
 import Link from 'next/link';
-import { useTranslations } from 'next-intl';
 
 const Custom500: React.FC = () => {
-    const t = useTranslations('errors.500');
     return (
         <div>
-            <h1>{t('title')}</h1>
+            <h1>500 - Internal Server Error</h1>
             <p>Something went wrong on our end. Please try again later.</p>
             <Link href="/">
                 Go back home
--- app/[locale]/account/connections/page.tsx
+++ app/[locale]/account/connections/page.tsx
@@ -1,7 +1,6 @@
 'use client'
 
 import React from 'react';
-import { useTranslations } from 'next-intl';
 import sanitizeHtml from 'sanitize-html';
 import ConnectionSection from '@/components/ConnectionSection';
 import ContentContainer from '@/components/ContentContainer';
@@ -11,7 +10,6 @@ const REDIRECT_URI = process.env.NEXT_PUBLIC_ORCID_DEV_CALLBACK;
 const ORCID_CLIENT_ID = process.env.NEXT_PUBLIC_ORCID_CLIENT_ID;
 
 const ConnectionsPage: React.FC = () => {
-  const t = useTranslations('Connections');
   // Sandbox Uri
   //const orcidUri = `https://sandbox.orcid.org/oauth/authorize?client_id=${orcidClientId}&response_type=code&scope=/read-limited&redirect_uri=${redirectURI}`;
 
@@ -19,25 +17,9 @@ const ConnectionsPage: React.FC = () => {
   const orcidUri = `https://orcid.org/oauth/authorize?client_id=${ORCID_CLIENT_ID}&response_type=code&scope=/authenticate&redirect_uri=${REDIRECT_URI}`;
 
   const orcidContentString = sanitizeHtml('ORCID provides a persistent identifier - an ORCID iD - that distinguishes you from other users. Learn more at <a href="https://orcid.org/" target="_blank" rel="noopener noreferrer">ORCID.org</a>.');
-  const name = 'Juliet'
   return (
     <PageWrapper title={'Connections'}>
-      {/*-Testing translation capabilities*/}
-      <h1>{t('title')}</h1>
-
-      <p>{t('content', { name })}</p>
-      <p>{t('itemCount', { count: 1 })}</p>
-      <p>
-        {t.rich('message', {
-          guidelines: (chunks) => <a href="/guidelines">{chunks}</a>
-        })}
-      </p>
-      {t.rich('markup', {
-        important: (chunks) => <b>${chunks}</b>
-      })}
-      <div dangerouslySetInnerHTML={{ __html: t.raw('raw') }} />
-
-      {/*End of Testing translation capabilities */}
+      <h1>My Connections</h1>
       <ContentContainer>
         <ConnectionSection
           type='orcid'
