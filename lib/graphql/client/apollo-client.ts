b97f36c09a1646b94bbb9c5135ae95fb4074c901
Juliet Shin <juliet.shin@ucop.edu>
2024-11-20 07:00:45 -0800

    removed duplicate .env variables

--- lib/graphql/client/apollo-client.ts
+++ lib/graphql/client/apollo-client.ts
@@ -3,7 +3,7 @@ import { authLink, retryLink } from "../graphqlHelper";
 
 export const createApolloClient = () => {
     const httpLink = createHttpLink({
-        uri: `${process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT}/graphql`
+        uri: `${process.env.NEXT_PUBLIC_SERVER_ENDPOINT}/graphql`
     });
     return new ApolloClient({
         link: from([authLink, retryLink, httpLink]),
