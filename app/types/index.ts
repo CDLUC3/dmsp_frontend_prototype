f6285f2a44e03b1e70a101d32f393c255499addb
Juliet Shin <juliet.shin@ucop.edu>
2025-02-18 12:14:38 -0800

    Added optional description param to RadioGroupComponent

--- app/types/index.ts
+++ app/types/index.ts
@@ -203,10 +203,11 @@ export interface Affiliation {
 interface RadioButtonInterface {
   value: string;
   label: string;
-  description: string;
+  description?: string;
 }
 export interface RadioButtonProps {
   name: string;
+  description?: string;
   radioGroupLabel: string;
   radioButtonData: RadioButtonInterface[];
   value: string;
@@ -214,6 +215,7 @@ export interface RadioButtonProps {
   errorMessage?: string;
   onChange?: (value: string) => void;
 }
+
 interface CheckboxInterface {
   value: string;
   label: string;
