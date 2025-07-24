dd865c567acda9ee78ea3e2bb72d1101f1b9d01a
Juliet Shin <juliet.shin@ucop.edu>
2025-07-23 17:25:02 -0700

    removed two of the events that I wasnt using

--- components/TinyMCEEditor/index.tsx
+++ components/TinyMCEEditor/index.tsx
@@ -61,7 +61,7 @@ const TinyMCEEditor = ({ content, setContent, onChange, error, id, labelId, help
           body { font-family: "Poppins", sans-serif; color:#393939;};
           `,
           setup: (editor: TinyMCEEditorType) => {
-            editor.on('Change KeyUp Input Blur', () => {
+            editor.on('Change KeyUp', () => {
               setContent(editor.getContent());
               onChange?.();
               // Close all remaining open menus when content changes. 
