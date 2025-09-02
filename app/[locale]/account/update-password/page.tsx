d25acd1bc7b76792a33177ae0ba9d13376920508
Fraser Clark <me@fraserclark.com>
2025-09-02 16:51:09 +0200

    fix linting issue

--- app/[locale]/account/update-password/page.tsx
+++ app/[locale]/account/update-password/page.tsx
@@ -1,11 +1,10 @@
 'use client'
 
 import React from 'react';
-import { useTranslations } from 'next-intl';
 import PageHeader from '@/components/PageHeader';
 
 const UpdatePasswordPage: React.FC = () => {
-  const t = useTranslations('Account');
+
 
   return (
     <>
