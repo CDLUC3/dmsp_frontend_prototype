ba5e17ed4358a94c614044eca457bb5d9008338e
Juliet Shin <juliet.shin@ucop.edu>
2026-03-31 10:40:44 -0700

    removed console.log

--- app/[locale]/template/customizations/[templateCustomizationId]/page.tsx
+++ app/[locale]/template/customizations/[templateCustomizationId]/page.tsx
@@ -93,7 +93,6 @@ const TemplateCustomizationOverview: React.FC = () => {
     variables: { templateCustomizationId: Number(templateCustomizationId) },
     fetchPolicy: 'cache-and-network', // User sees cached data imediately while a fresh fetch runs to get latest updates from editing
   });
-  console.log("***Template Customization Overview Data***", data);
 
   // Mutations
   const [publishTemplateCustomization] = useMutation(PublishTemplateCustomizationDocument);
